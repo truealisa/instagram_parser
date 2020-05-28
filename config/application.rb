87da2d23ffcae83553457d46a8222a6466c93ece
Alisa Truzhenko <truealisa@gmail.com>
2020-05-28 22:51:37 +0300

    Add instagram_credentials config

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module InstagramParser
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.instagram_credentials = YAML.load_file(Rails.root.join('config/instagram_credentials.yml')).deep_symbolize_keys!
   end
 end
